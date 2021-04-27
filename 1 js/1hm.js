a9782023b7e04d1509321b56f9a592dddefb577e
nekosiak <81107678+nekosiak@users.noreply.github.com>
2021-04-27 18:04:23 +0300

    Add files via upload

---
+++ 1 js/1hm.js
@@ -0,0 +1,11 @@
+let age = prompt('How old are u?', 100);
+let name = prompt('Whats your name?');
+if(age<18) alert('You are not allowed to visit this website');
+else if (age>17 && age <23) 
+{
+    let isBoss = confirm("Are you sure you want to continue?"); 
+    if(isBoss==true) alert(`Welcome,  ${name}`);
+    else alert('You are not allowed to visit this website');
+}
+else alert(`Welcome, ${name}`);
+  
