916a82f5466931c860600b02803d025f405e2451
nekosiak <81107678+nekosiak@users.noreply.github.com>
2021-04-27 18:13:26 +0300

    Update 1hm.js

--- 1 js/1hm.js
+++ 1 js/1hm.js
@@ -1,7 +1,7 @@
 let age = prompt('How old are u?', 100);
 let name = prompt('Whats your name?');
 if(age<18) alert('You are not allowed to visit this website');
-else if (age>17 && age <23) 
+else if (age>=18 && age <=22) 
 {
     let isBoss = confirm("Are you sure you want to continue?"); 
     if(isBoss==true) alert(`Welcome,  ${name}`);
